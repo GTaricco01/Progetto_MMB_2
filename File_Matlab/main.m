41e7999b8225fad9b0aa6f77e4e15734e9e72948
GTaricco01 <s281770@studenti.polito.it>
2024-12-29 09:24:31 +0100

    progressi con modello ODE. Ancora da iniziare implementazione modello cinetico

--- File_Matlab/main.m
+++ File_Matlab/main.m
@@ -23,25 +23,33 @@ N_class = [441148; 5666380; 5962570; 6570438; 8061697; 9619515; 7964817; 6141543
 N       = sum(N_class);       % Total population N = S + I + R
 I0      = confirmed(1);       % initial number of infected (dai dati reali)
 beta    = 8.45e-9;            % rate of infection
-gamma   = 0.25;               % rate of recovery 
+gamma   = 0.24;               % rate of recovery 
 delta   = 0;                % rate of immunity loss
 T       = length(timeVector); % same period of the real data [T] = [days]
 % Inserire calcolo R0
 R0      = beta*(N-I0)/gamma;
 fprintf('Valore parametro R0 %.2f\n', R0)
-X0 = [N,I0,0];
+X0   = [N,I0,0];
 X0_c = zeros(9,3);
+I0_c = zeros(9,1); I0_c(8) = I0;
 % X0(:,1) = colonna dei suscettibili iniziali divisi per classe di età
 % X0(:,2) = colonna degli infetti iniziali divisi per classe di età
 % X0(:,3) = colonna dei rimossi iniziali divisi per classe di età
 
 X0_c(:,1) = N_class;
-X0_c(:,2) = I0;
+X0_c(:,2) = I0_c;
+
+% supponiamo di avere come infetti iniziali solo 2 individui della classe
+% numero 8: per avere un'evoluzione significativa dell'epidemia nel tempo
+% ho bisogno di implementare il contagio inter classe.
+
+
 %%
 p      = [beta, gamma];
 % chiamo la funzione ode45 una volta per ogni classe
 [t, X]     = ode45(@(t,x)sir_model(t,x,p,1,eye(9)),[0 T-1],X0(1,:));
-[t_c, X_c] = ode45(@(t,x)sir_model(t,x,p,1,M),[0 T-1],X0_c(1,:));
+% M(1:3,1:3) = M(1:3,1:3)./2;
+[t_c, X_c] = ode45(@(t,x)sir_model(t,x,p,8,M),[0 T-1],X0_c(8,:));
 S         = X(:,1);
 I         = X(:,2);
 R         = X(:,3);
@@ -58,3 +66,4 @@ subplot(1,2,2)
 plot(t_c,I_c,'r','LineWidth',2);
 xlabel('Days'); ylabel('Number of individuals'); title('Infected SIR age model')
 set(gca,'FontSize',10)
+
--- File_Matlab/sir_model.m
+++ File_Matlab/sir_model.m
@@ -1,6 +1,6 @@
-function dx = sir_model(~,x,p,class,M) % x = [variabili], p =[parametri]
-dx(1) = -p(1)*x(1)*sum((M(:,class)*x(2)));
-dx(2) = p(1)*x(1)*x(2) - p(2)*x(2);
+function dx = sir_model(~,x,p,class,M) % x = [variabili], p =[beta,gamma]
+dx(1) = -p(1)*x(1)*sum((M(class,:)*x(2)));
+dx(2) = p(1)*x(1)*sum((M(class,:)*x(2))) - p(2)*x(2);
 dx(3) = p(2)*x(2);
 dx = dx';
 end
